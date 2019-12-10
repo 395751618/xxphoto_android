3f19dedee09f14ffd2630ce0d6fbb1634e96eac0
xiangxia <xia.xiang@mindertech.co.uk>
2019-12-10 14:29:00 +0800

    init

--- app/src/main/java/com/mindertech/xxphoto_android/MainActivity.java
+++ app/src/main/java/com/mindertech/xxphoto_android/MainActivity.java
@@ -1,21 +1,30 @@
 package com.mindertech.xxphoto_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.mindertech.xxphoto.main.XXPhotoMainUI;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_text)
+    TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @OnClick(R.id.tv_text)
--- xxphoto/src/main/java/com/mindertech/xxphoto/main/XXPhotoMainUI.java
+++ xxphoto/src/main/java/com/mindertech/xxphoto/main/XXPhotoMainUI.java
@@ -1,15 +1,19 @@
 package com.mindertech.xxphoto.main;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import com.mindertech.xxphoto.R;
 import com.mindertech.xxphoto.R2;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @project xxphoto_android
@@ -20,10 +24,28 @@ import butterknife.BindView;
  */
 public class XXPhotoMainUI extends FragmentActivity {
 
+    @BindView(R2.id.vp_content)
+    ViewPager vpContent;
+    @BindView(R2.id.fl_container)
+    FrameLayout flContainer;
+    @BindView(R2.id.tv_back)
+    TextView tvBack;
+    @BindView(R2.id.tv_album)
+    TextView tvAlbum;
+    @BindView(R2.id.layout_choose_album)
+    LinearLayout layoutChooseAlbum;
+    @BindView(R2.id.tv_upload)
+    TextView tvUpload;
+    @BindView(R2.id.bottom_toolbar)
+    FrameLayout bottomToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_xxmain);
+        ButterKnife.bind(this);
+
+
     }
 
     @Override
